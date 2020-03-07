68c92c1e8a0e7888528e06bd12be009fb16eaf60
Jefferson Barbosa <jeffersoncbarbosa@yahoo.com.br>
2020-03-07 20:57:44 -0300

    retirado

--- ler_pdf.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-FILE_PATH = 'C:/testes/email_download/DARE.pdf'
-
-with open(FILE_PATH, mode='rb') as f:
-    reader = PyPDF2.PdfFileReader(f)
-    page = reader.getPage(0)
-    print(page.extractText())
