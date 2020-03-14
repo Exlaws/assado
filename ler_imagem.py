30e491ea7ce077db3c548abf7d49b3011f524360
Jefferson Camargo Barbosa <61355495+Exlaws@users.noreply.github.com>
2020-03-14 16:34:41 -0300

    Delete ler_imagem.py
    not works...

--- ler_imagem.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-from PIL import Image
-
-pytesseract .pytesseract.tesseract_cdm = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-img = Image.open('image.jpg')
-text = pytesseract.image_to_string(img)
-print(text)
